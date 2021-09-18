9961c62649cd1934e6a86eab38bfb45a43753183
Ashish Sahu <aajisahu@gmail.com>
2021-09-18 09:43:29 +0530

    Play until player 1 win

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -6,9 +6,16 @@ public class SnakeLadder {
       
 		System.out.println("After roll the die You got : "+roll());
 		position();
+		repeatWin();
+		
+		
 	}
 	
-        public static int roll() {
+	
+     
+	
+	
+	public static int roll() {
             return (int) Math.floor(Math.random() * 6) + 1;
         }
       
@@ -32,5 +39,38 @@ public class SnakeLadder {
         	return getPosition ;
                 
         }
+        
+        
+       public static int repeatWin()
+       {
+    	   
+    	   int store = 0;
+    	   while (store <= 100)
+    	   {
+    		   
+    		   int roll=roll();
+    		   System.out.println("PLayer1 got : " + roll);
+    		   store += roll;
+    		   System.out.println("Player1 is now at : "+ store);
+    		   
+    		   if (store < 89 && store > 11)
+    		   {
+    			   switch (position ())
+    			   {
+    			   case 1:
+    				   store += roll;
+    				   System.out.println("Player1 got a ladder is now at : " + store);
+    				   
+    			   case 2:
+    				   store -= roll;
+                       System.out.println("Player1 got a snake is now at : " + store);
+    			   }
+    		   }
+    		 
+    		    
+    	   }
+    	   System.out.println("Player 1 has won");
+		    return store;
+       }
         
 }
