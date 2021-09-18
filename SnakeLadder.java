9d2e07c67cdf0f3115b2b8b0fc99574cc2aa371d
Ashish Sahu <aajisahu@gmail.com>
2021-09-18 11:07:45 +0530

    adding multiplayer in to SnakeLadder

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -1,39 +1,50 @@
 package com.snake_ladder;
 
 public class SnakeLadder {
-	private static int count = 0;
+	private static int count = 0,count2 = 0;
 	
 	public static void main(String[] args) {
       
 		position();
+		position2();
 		repeatWin();
 		System.out.println("Player 1 has won with a total dice roll of : "+count);	 
 		
-		
 	}
 		
+	//player 1 roll
 	public static int roll() {
 		count ++;
             return (int) Math.floor(Math.random() * 6) + 1;
         }
-      
+	//player 2  roll
+	public static int roll2() {
+		count2 ++;
+            return (int) Math.floor(Math.random() * 6) + 1;
+        }
+	
+	//player 1  roll
      public static int position () {
         
         	int getPosition= (int) Math.floor(Math.random() * 3);
           	return getPosition ;
                 
         }
-
-        
+   //player 2  roll
+     public static int position2 () {
+         
+     	int getPosition= (int) Math.floor(Math.random() * 3);
+       	return getPosition ;
+     }
+     
        public static int repeatWin()
        {
     	   
-    	   int store = 1;
-    	   int win;
-    	   while (store <= 100 )
+    	   int store =0;
+    	   int store1 =0;
+    	   while (store <= 100 && store1 <= 100)
     	   {
-    		   win=store;
-    		      		   
+    		      		      		   
     		   int roll=roll();
     		   System.out.println("PLayer1 got : " + roll);
     		   
@@ -61,10 +72,46 @@ public class SnakeLadder {
     			   }
     		   }
     		 
-    		    
-    	   }
-    	   System.out.println("Player 1 has won");
-		    return store;
-       }
-        
+    		   //for player 2
+    		   int roll2 = roll2();
+               System.out.println("Player2 got : " + roll2);
+               
+               store1 += roll2;
+               System.out.println("Player2 is now at : " + store1);
+               
+               if (store1 > 100) {
+                   store1 -= roll2;
+                   System.out.println("Player 2 can not surpass 100 and is reverted back to : " + store1);
+               }
+               
+               if (store1 < 89 && store1 > 11) {
+                   switch (position2()) {
+                       
+                   case 1 :
+                           store1 += roll2;
+                           System.out.println("Player2 got a ladder is now at : " + store1);
+                       
+                       case 2 :
+                           store1 -= 2 * roll2;
+                           System.out.println("Player2 got a snake is now at : " + store1);
+                          
+                   }
+                   
+               }
+               
+  	   }
+    	   System.out.println("Player 1 finished in : " + store);
+           System.out.println("Player 2 finished in : " + store1);
+           if (store > store1) {
+        	   
+               System.out.println("Player 1 has won the game. \nThe total number of time the dice was rolled per player is : " + count);
+           } 
+           else {
+        	   
+               		System.out.println("Player 2 has won the game. The total number of time the dice was rolled per player is : " + count);
+           		}
+    	   return store;
+    	   
+    	   
+   }
 }
