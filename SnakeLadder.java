fbd57510a1b025c20396f578d339e1df6e987ca3
Ashish Sahu <aajisahu@gmail.com>
2021-09-18 10:22:05 +0530

    adding exact 100 position

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -4,66 +4,57 @@ public class SnakeLadder {
 
 	public static void main(String[] args) {
       
-		System.out.println("After roll the die You got : "+roll());
 		position();
 		repeatWin();
 		
 		
 	}
-	
-	
-     
-	
-	
+		
 	public static int roll() {
             return (int) Math.floor(Math.random() * 6) + 1;
         }
       
-        
-        public static int position () {
+     public static int position () {
         
         	int getPosition= (int) Math.floor(Math.random() * 3);
-        	
-        	switch ( getPosition ) {
-        	
-        	case 0:
-        		System.out.print("Your Position is NoPlay ");
-        		break;
-        	case 1:
-        		System.out.print("Your Position is Ladder ");
-        		break;
-        	default:
-        		System.out.print("Your Position is Snake  ");
-        		break;
-        	}
-        	return getPosition ;
+          	return getPosition ;
                 
         }
-        
+
         
        public static int repeatWin()
        {
     	   
-    	   int store = 0;
-    	   while (store <= 100)
+    	   int store = 1;
+    	   int win;
+    	   while (store <= 100 )
     	   {
-    		   
+    		   win=store;
+    		      		   
     		   int roll=roll();
     		   System.out.println("PLayer1 got : " + roll);
-    		   store += roll;
-    		   System.out.println("Player1 is now at : "+ store);
     		   
+    		   store += roll;
+    		   if (store == 94 || store == 95 || store == 96 || store == 97 || store == 98 || store == 99 ) {
+    			   store=100;
+    			   System.out.println("Player1 is now at : "+ store);
+    			   break;
+    		   }
+    		   
+    		   else
+    			   System.out.println("Player1 is now at : "+ store);
+    		     		   
     		   if (store < 89 && store > 11)
     		   {
     			   switch (position ())
     			   {
     			   case 1:
     				   store += roll;
-    				   System.out.println("Player1 got a ladder is now at : " + store);
+    				   System.out.println("Player 1 got a ladder is now at : " + store);
     				   
     			   case 2:
     				   store -= roll;
-                       System.out.println("Player1 got a snake is now at : " + store);
+                       System.out.println("Player 1 got a snake is now at : " + store);
     			   }
     		   }
     		 
