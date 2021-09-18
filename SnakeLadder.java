23d68f8ed2435c3f89b1119b714b1033ea8c529f
Ashish Sahu <aajisahu@gmail.com>
2021-09-18 09:04:02 +0530

    starting player position with zero

---
+++ SnakeLadder.java
@@ -0,0 +1,10 @@
+package com.snake_ladder;
+
+public class SnakeLadder {
+
+	public static void main(String[] args) {
+        int position = 0;
+        System.out.println("Your position is: "+ position);
+	}
+	
+}
