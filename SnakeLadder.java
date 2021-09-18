5c3ea59fda084a5bc0aeaffc72245ffeb23d9de5
Ashish Sahu <aajisahu@gmail.com>
2021-09-18 10:32:14 +0530

    adding total number of roll dice to win the game

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -1,16 +1,19 @@
 package com.snake_ladder;
 
 public class SnakeLadder {
-
+	private static int count = 0;
+	
 	public static void main(String[] args) {
       
 		position();
 		repeatWin();
+		System.out.println("Player 1 has won with a total dice roll of : "+count);	 
 		
 		
 	}
 		
 	public static int roll() {
+		count ++;
             return (int) Math.floor(Math.random() * 6) + 1;
         }
       
