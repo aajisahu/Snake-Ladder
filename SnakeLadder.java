6c7bd2e86b58d56dfd9aac0d6f2f5b5c192299e8
Ashish Sahu <aajisahu@gmail.com>
2021-09-18 09:29:04 +0530

    checking the positon whether the player is No Play, Ladder or Snake,

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -5,15 +5,32 @@ public class SnakeLadder {
 	public static void main(String[] args) {
       
 		System.out.println("After roll the die You got : "+roll());
-    
-		
+		position();
 	}
 	
         public static int roll() {
             return (int) Math.floor(Math.random() * 6) + 1;
         }
+      
         
+        public static int position () {
         
-	
-	
+        	int getPosition= (int) Math.floor(Math.random() * 3);
+        	
+        	switch ( getPosition ) {
+        	
+        	case 0:
+        		System.out.print("Your Position is NoPlay ");
+        		break;
+        	case 1:
+        		System.out.print("Your Position is Ladder ");
+        		break;
+        	default:
+        		System.out.print("Your Position is Snake  ");
+        		break;
+        	}
+        	return getPosition ;
+                
+        }
+        
 }
